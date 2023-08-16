f26174e900bc92edee547e367d087cdd82c0b61d
ReloJunior <andresrodriguezab@hotmail.com>
2023-08-16 17:28:23 -0300

    Nuevo repo para poder hacer un deploy con github pages

---
+++ README.md
@@ -0,0 +1,9 @@
+# Crear un TodoMVC con TypeScript
+
+- [x] Inicializar proyecto con Vite
+- [x] Añadir Linter para TypeScript + React
+- [x] Añadir estilos del TodoMVC
+- [x] Listar todos los TODOs
+- [x] Poder borrar un TODO
+- [x] Marcar TODO como completado
+- [] Añadir forma de filtrar TODOs (Footer)
---
+++ src/mocks/todos.ts
@@ -0,0 +1,53 @@
+export const mockAllCompletedTodos = [
+    {
+      completed: true,
+      id: '7b6d5f38-e510-4409-aeb0-1f6f6422384e',
+      title: 'Finalizar el curso PotenciAR'
+    },
+    {
+      completed: true,
+      id: 'efad0afc-7d2e-4020-8ef4-14fd0b832de8',
+      title: 'Aprender React + TypeScript'
+    },
+    {
+      completed: true,
+      id: '6a3d0d0f-d2d6-4d2a-9b08-5a5d8a5e0c1d',
+      title: 'Ir al gimnasio'
+    }
+  ]
+  
+  export const mockAllActiveTodos = [
+    {
+      completed: false,
+      id: 'c19f8c9b-ae32-4c8a-9bed-d141b09f5477',
+      title: 'Hacer deporte una vez a la semana'
+    },
+    {
+      completed: false,
+      id: 'efad0afc-7d2e-4020-8ef4-14fd0b832de8',
+      title: 'Seguir haciendo canciones y mejorar mi musica'
+    },
+    {
+      completed: false,
+      id: '6a3d0d0f-d2d6-4d2a-9b08-5a5d8a5e0c1d',
+      title: 'Crear un portfolio personal'
+    }
+  ]
+  
+  export const mockTodos = [
+    {
+      completed: false,
+      id: 'c19f8c9b-ae32-4c8a-9bed-d141b09f5477',
+      title: 'Sacar los perros a pasear'
+    },
+    {
+      completed: true,
+      id: 'efad0afc-7d2e-4020-8ef4-14fd0b832de8',
+      title: 'Comprar chocolates'
+    },
+    {
+      completed: false,
+      id: '6a3d0d0f-d2d6-4d2a-9b08-5a5d8a5e0c1d',
+      title: 'Seguir intentando buscar trabajo'
+    }
+  ]
